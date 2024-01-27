8d31e18fab90059afb5b1e203b032633f682428f
Laxmirawat0720 <155939431+Laxmirawat0720@users.noreply.github.com>
2024-01-27 20:11:56 +0530

    Add files via upload

---
+++ arithmetic operations.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cout<<"enter two numbers";
+	cin>>num1>>num2;
+	cout<<"sum is"<<num1+num2;
+	cout<<"diff is"<<num1-num2;
+	cout<<"product is"<<num1*num2;
+	cout<<"quotient is"<<num1/num2;
+	cout<<"remainder is"<<num1%num2;
+	return 0;
+}
